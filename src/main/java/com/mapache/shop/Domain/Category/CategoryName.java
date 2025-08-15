9ff466ed18053c0df2dfca4d338f5ba2deb0a786
Roberto González Pérez <mrmapache@192.168.1.69>
2025-08-15 14:11:07 -0600

    Add init creation

---
+++ src/main/java/com/mapache/shop/Domain/Category/CategoryName.java
@@ -0,0 +1,50 @@
+package com.mapache.shop.Domain.Category;
+
+import java.util.Objects;
+
+public class CategoryName {
+
+    private final String value;
+    private static final String regex = "^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{3,50}$";
+
+
+    public CategoryName(String categoryName)
+    {
+        if(categoryName == null || categoryName.isBlank())
+        {
+            throw new IllegalArgumentException("Category name cant be null or empty");
+        }
+        if(!categoryName.matches(regex))
+        {
+            throw new IllegalArgumentException("Category name must contain only letters and spaces, max 50 characters, minimum 3.");
+        }
+
+        this.value = categoryName;
+    }
+
+    public String value()
+    {
+        return value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return value;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if(this == o) return true;
+        if(!(o instanceof CategoryName categoryName)) return false;
+
+        return Objects.equals(value, categoryName.value);
+    }
+}
---
+++ src/main/java/com/mapache/shop/Domain/Product/Money.java
@@ -0,0 +1,74 @@
+package com.mapache.shop.Domain.Product;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Money {
+
+    private final BigDecimal amount;
+    private final String currency;
+
+    public Money(BigDecimal incomingAmount, String incomingCurrency){
+        if(incomingAmount == null)
+        {
+            throw new IllegalArgumentException("Amount cant be null.");
+        }
+        if(incomingAmount.compareTo(BigDecimal.ZERO) < 0)
+        {
+            throw new IllegalArgumentException("Amount cant be negative.");
+        }
+        if(incomingCurrency == null || incomingCurrency.isBlank())
+        {
+            throw new IllegalArgumentException("Currency cannot be null.");
+        }
+        this.amount = incomingAmount;
+        this.currency = incomingCurrency;
+    }
+
+    public Money add(Money other)
+    {
+        validateSameCurrency(other);
+        return new Money(this.amount.add(other.amount), this.currency);
+    }
+
+    public Money subtract(Money other)
+    {
+        validateSameCurrency(other);
+        return new Money(this.amount.subtract(other.amount), this.currency);
+    }
+
+    public Money multiply(BigDecimal factor)
+    {
+        return new Money(this.amount.multiply(factor), this.currency);
+    }
+
+    private void validateSameCurrency(Money other)
+    {
+        if(!this.currency.equals(other.currency))
+            throw new IllegalArgumentException("Currencies are not the same.");
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if(!(o instanceof Money money)) return false;
+
+        return amount.compareTo(money.amount) == 0 &&
+                Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public String toString()
+    {
+        return amount + " " + currency;
+    }
+}
