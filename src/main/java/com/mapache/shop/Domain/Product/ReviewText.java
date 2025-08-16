982bf1eb091bc1e67793689966599a3a4d8a7505
Roberto González Pérez <mrmapache@192.168.1.69>
2025-08-16 00:56:20 -0600

    ADD ReviewText to Domain

--- src/main/java/com/mapache/shop/Domain/Product/ReviewText.java
+++ src/main/java/com/mapache/shop/Domain/Product/ReviewText.java
@@ -1,4 +1,46 @@
 package com.mapache.shop.Domain.Product;
 
+import java.util.Objects;
+
 public class ReviewText {
+
+    private final String value;
+
+    public ReviewText(String incomingText)
+    {
+        if(incomingText == null || incomingText.isBlank())
+        {
+            throw new IllegalArgumentException("Review cannot be null");
+        }
+        if(incomingText.length() < 50)
+        {
+            throw new IllegalArgumentException("Review must have at least a length of 50 characters.");
+        }
+        if (incomingText.length() > 1000) {
+            throw new IllegalArgumentException("Review cannot exceed 1000 characters.");
+        }
+
+
+        this.value = incomingText;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if(this == o) return true;
+        if(!(o instanceof ReviewText review)) return false;
+
+        return Objects.equals(value, review.value);
+    }
 }
